79c460448b0ffb2073201c1a91664ab5a3a18e93
daviddprs <144987286+daviddprs@users.noreply.github.com>
2026-07-21 12:18:13 +0700

    Initial commit

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Pertamaku</title>
+</head>
+<body>
+    <h1>Halo Laravel!</h1>
+    <p>Project pertamaku berhasil berjalan.</p>
+</body>
+</html>
